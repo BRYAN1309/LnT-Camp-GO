97fbff92e1390e1dbbf0975edd06e3dd6ee87243
BRYAN1309 <bryanthanaya1@gmail.com>
2025-07-10 20:11:42 +0700

    Variables

--- main.go
+++ main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var (
+		nama string
+		umur int
+	)
+	const pi = 3.14
+
+	nama = "Bryan"
+	umur = 20
+	fmt.Println("nama:", nama)
+	fmt.Println("umur:", umur)
+	fmt.Println("pi:", pi)
 }
