6ebf5009b0eb93a45e60b4fdadc35e1b8ef0e15d
BRYAN1309 <bryanthanaya1@gmail.com>
2025-07-10 20:24:21 +0700

    Input output

--- main.go
+++ main.go
@@ -3,17 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
 
-	var (
-		nama string
-		umur int
-	)
-	const pi = 3.14
+	// var (
+	// 	nama string
+	// 	umur int
+	// )
+	// const pi = 3.14
 
-	nama = "Bryan"
-	umur = 20
-	fmt.Println("nama:", nama)
-	fmt.Println("umur:", umur)
-	fmt.Println("pi:", pi)
+	// nama = "Bryan"
+	// umur = 20
+	// fmt.Println("nama:", nama)
+	// fmt.Println("umur:", umur)
+	// fmt.Println("pi:", pi)
+
+	fmt.Println("===Program Input Output===")
+	var nama string
+	fmt.Print("Masukkan nama: ")
+	fmt.Scanln(&nama)
+	fmt.Println(nama)
+
+	var age int
+	fmt.Print("Masukkan umur: ")
+	fmt.Scanln(&age)
+	fmt.Println(age)
+
+	fmt.Println("Summary")
+	fmt.Printf("Halo nama saya adalah %s dan umur saya adalah %d", nama, age)
 }
